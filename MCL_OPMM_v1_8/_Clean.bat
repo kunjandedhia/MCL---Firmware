acd48497905de8be322b0bf64b4c70e1737aab8f
acevinsolutions <k.mhatre@acevin.com>
2017-03-16 19:49:55 +0530

    OPMM_V1_8
    Final OPMM Version

---
+++ MCL_OPMM_v1_8/_Clean.bat
@@ -0,0 +1,19 @@
+Del *.uvgui.* /Q
+Del *.uvguix.* /Q
+Del *.uvgui_* /Q
+Del *.bak /Q
+Del "RTE\*.bak" /Q
+Del "Source C Files\*.bak" /Q
+Del "OP_Include\*.bak" /Q
+Del "Objects\*.__i" /Q
+Del "Objects\*.CRF" /Q
+Del "Objects\*.D" /Q
+Del "Objects\*.O" /Q
+Del "Objects\*._ia" /Q
+Del "Objects\*.axf" /Q
+Del "Objects\*.DEP" /Q
+Del "Objects\*.iex" /Q
+Del "Objects\*.lnp" /Q
+Del "OP_Include\*.tmp" /Q
+Del "OP_Include\*.bak" /Q
+Del "OP_Source\*.bak" /Q
